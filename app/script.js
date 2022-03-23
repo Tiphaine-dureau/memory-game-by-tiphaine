fafbda99b69fa960d900b5e12d121b955751228f
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2022-03-23 16:10:04 +0100

    -Ajout script compteur de temps au dessus de la progress barre

--- app/script.js
+++ app/script.js
@@ -5,18 +5,25 @@ $(document).ready(() => {
         percent = 100;
         $('#start').attr('disabled', true);
         timerId = setInterval(function () {
-            // increment progress bar
+            // decrement progress bar
             percent -= 1;
             $('#load').html(percent + '%').css('width', percent + '%');
-
             if (percent <= 10) {
-                $('.progress-bar').css('color', 'red');
+                $('.progress-bar').addClass('progress-bar-last-seconds').removeClass('progress-bar');
             }
-            if (percent === 0){
+            if (percent === 0) {
                 clearInterval(timerId);
                 $('#start').attr('disabled', false);
             }
         }, 1000);
-})});
-
-
+        let timeLeft = percent;
+        let downloadTimer = setInterval(function () {
+            timeLeft -= 1;
+            $('#countdown').html(timeLeft + ' secondes restantes');
+            if (timeLeft <= 0) {
+                clearInterval(downloadTimer);
+                $('#countdown').html('Temps écoulé !');
+            }
+        }, 1000);
+    })
+});

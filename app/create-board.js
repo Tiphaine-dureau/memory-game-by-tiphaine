73c1ee00873e815910d28431e0fdceb08e7be3ab
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2022-03-26 19:11:49 +0100

    refactor du board game

---
+++ app/create-board.js
@@ -0,0 +1,50 @@
+/**
+ * Affiche dynamiquement et aléatoirement les cases du plateau de jeu
+ */
+function createBoardGame() {
+    shuffleCards(easyBoardCards);
+    const boardSize = Math.sqrt(easyBoardCards.length);
+    createRows(boardSize);
+}
+
+function createRows(boardSize) {
+    for (let i = 0; i < boardSize; i++) {
+        createRow(boardSize, i);
+    }
+}
+
+function createRow(boardSize, i) {
+    const rowId = `row-${i}`;
+    const rowElement = `<div class="row" id="${rowId}"></div>`;
+    $('#game-board').append(rowElement);
+    createCols(boardSize, i, rowId);
+}
+
+function createCols(boardSize, i, rowId) {
+    for (let j = 0; j < boardSize; j++) {
+        createCol(j, i, rowId, boardSize);
+    }
+}
+
+function createCol(j, i, rowId, boardSize) {
+    const colId = `col-${i}-${j}`;
+    const colElement = `<div class="col" id="${colId}"></div>`;
+    const rowIdSelector = `#${rowId}`;
+    $(rowIdSelector).append(colElement);
+    const colIdSelector = `#${colId}`;
+    const currentCardId = (i * boardSize) + j;
+    const cardImgSrc = easyBoardCards[currentCardId].img;
+    const imgElement = `<img src=${cardImgSrc} alt="images du jeu"/>`;
+    $(colIdSelector).append(imgElement);
+}
+
+/**
+ * Mélange les cartes en utilisant l'algo de Durstenfeld
+ * @param cards
+ */
+function shuffleCards(cards) {
+    for (let i = cards.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [cards[i], cards[j]] = [cards[j], cards[i]];
+    }
+}
---
+++ app/data.js
@@ -0,0 +1,83 @@
+// Déclaration du tableau de cartes
+const easyBoardCards = [
+    {
+        id: 1,
+        name: 'butterfly',
+        img: 'assets/images/img-easy/butterfly.svg',
+    },
+    {
+        id: 2,
+        name: 'elephant',
+        img: 'assets/images/img-easy/elephant.svg'
+    },
+    {
+        id: 3,
+        name: 'flamingo',
+        img: 'assets/images/img-easy/flamingo.svg'
+    },
+    {
+        id: 4,
+        name: 'frog',
+        img: 'assets/images/img-easy/frog.svg'
+    },
+    {
+        id: 5,
+        name: 'koala',
+        img: 'assets/images/img-easy/koala.svg'
+    },
+    {
+        id: 6,
+        name: 'octopus',
+        img: 'assets/images/img-easy/octopus.svg'
+    },
+    {
+        id: 7,
+        name: 'sperm-whale',
+        img: 'assets/images/img-easy/whale.svg'
+    },
+    {
+        id: 8,
+        name: 'tortoise',
+        img: 'assets/images/img-easy/tortoise.svg'
+    },
+    {
+        id: 9,
+        name: 'butterfly',
+        img: 'assets/images/img-easy/butterfly.svg',
+    },
+    {
+        id: 10,
+        name: 'elephant',
+        img: 'assets/images/img-easy/elephant.svg'
+    },
+    {
+        id: 11,
+        name: 'flamingo',
+        img: 'assets/images/img-easy/flamingo.svg'
+    },
+    {
+        id: 12,
+        name: 'frog',
+        img: 'assets/images/img-easy/frog.svg'
+    },
+    {
+        id: 13,
+        name: 'koala',
+        img: 'assets/images/img-easy/koala.svg'
+    },
+    {
+        id: 14,
+        name: 'octopus',
+        img: 'assets/images/img-easy/octopus.svg'
+    },
+    {
+        id: 15,
+        name: 'sperm-whale',
+        img: 'assets/images/img-easy/whale.svg'
+    },
+    {
+        id: 16,
+        name: 'tortoise',
+        img: 'assets/images/img-easy/tortoise.svg'
+    },
+]
